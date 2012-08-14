db42a38d9be859d51310556a080b2ce433e0e65c
Rodrigo Urubatan <rodrigo@urubatan.com.br>
2012-08-14 19:30:41 -0300

    almost done?

--- lib/twitsort/searcher.rb
+++ lib/twitsort/searcher.rb
@@ -1,4 +1,6 @@
+# encoding: utf-8
 require 'twitter'
+require 'pry'
 
 module Twitsort
   class Searcher
@@ -9,16 +11,26 @@ module Twitsort
 
     def search
       res = []
-      query = Twitter.search(@text)
-      query.results.each do |status|
-        res = {:from_user => status.from_user, :from_user_name => status.from_user_name}
-      end
-
+      query = nil
+      current_page = 1
+      begin
+        query = Twitter.search(@text, :rpp => 100, :page => current_page)
+        query.results.each do |status|
+          res << {:from_user => status.from_user, :from_user_name => status.from_user_name}
+        end
+        current_page = query.next_page.gsub(/.*page=([0-9]+).*/,'\1') if query.next_page
+      end while query.next_page
       res.select {|stat| following_me?(stat[:from_user]) }
     end
-
+    def any_random
+      res = search
+      res[rand(res.length)]
+    end
     def following_me?(name)
       true
     end
   end
 end
+s = Twitsort::Searcher.new('urubatan', 'Quero ganhar o livro do @urubatan autografado (http://bit.ly/OulSOF), quer ganhar também? da uma olhada neste link http://bit.ly/MVXl5b')
+result = s.any_random
+puts "@#{result[:from_user]} - #{result[:from_user_name]}"
